96045bbd8fcc967537247d838ac1a0ed8ef94977
ann_shcher <annnuta_2011@ukr.net>
2015-12-17 23:50:45 +0200

    form is showing

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,3 +1,17 @@
-public class MainForm {
+import javax.swing.*;
 
+class MainForm extends JFrame {
+
+    private JPanel panel1;
+
+    public MainForm(){
+        setContentPane(panel1);
+        setTitle("Media");
+        setSize(700,300);
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        new MainForm();
+    }
 }
