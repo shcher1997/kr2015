e3fc4038ecf337d55aaa6b2e131442fbeb8391fe
ann_shcher <annnuta_2011@ukr.net>
2015-12-19 00:28:26 +0200

    the main methods in the player added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,86 @@
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.PlaybackEvent;
+
 import javax.swing.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main extends JFrame {
+    FileInputStream fis;
+    BufferedInputStream bis;
+    public Player player;
+    public long pauseLocation;
+    public long songTotalLength;
+    public  String fileLocation;
+
+    public void stop(){
+        if (player!=null){
+            player.close();
+        }
+    }
+
+
+    public void pause(){
+        if (player!=null){
+            try {
+                pauseLocation = fis.available();
+                player.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void play(String path){
+        try {
+            fis = new FileInputStream(path);
+            bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+            songTotalLength = fis.available();
+            fileLocation = path + "";
+        } catch (FileNotFoundException | JavaLayerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(){
+            public void run(){
+                try {
+                    player.play();
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public void resume(){
+        try {
+            fis = new FileInputStream(fileLocation);
+            bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+            fis.skip(songTotalLength - pauseLocation);
+        } catch (FileNotFoundException | JavaLayerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(){
+            public void run(){
+                try {
+                    player.play();
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
     public static void main (String[] args) {
         new MainForm();
     }
